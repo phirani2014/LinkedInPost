026fc7a6df38b9bbb48af2921d363ce49f93208e
phirani2014 <phirani2014@gmail.com>
2020-10-31 12:11:16 +0000

    Initial Migration

---
+++ SharePostsTutorial/GoogleLang.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharePostsTutorial
+{
+    class GoogleLang
+    {
+        public string codelist()
+        {
+            var Code = new List<string>();
+            Code.Add("af");
+            Code.Add("ga");
+            Code.Add("sq");
+            Code.Add("it");
+
+            Code.Add("ja");
+
+            Code.Add("kn");
+            Code.Add("eu");
+            Code.Add("ko");
+            Code.Add("bn");
+            Code.Add("la");
+            Code.Add("be");
+            Code.Add("lv");
+            Code.Add("bg");
+            Code.Add("lt");
+            Code.Add("ca");
+            Code.Add("mk");
+            Code.Add("zh-CN");
+            Code.Add("ms");
+            Code.Add("zh-TW");
+            Code.Add("mt");
+            Code.Add("hr");
+            Code.Add("no");
+            Code.Add("cs");
+            Code.Add("fa");
+            Code.Add("da");
+            Code.Add("pl");
+            Code.Add("nl");
+            Code.Add("pt");
+            
+            Code.Add("ro");
+            Code.Add("eo");
+            Code.Add("ru");
+            Code.Add("et");
+            Code.Add("sr");
+            Code.Add("tl");
+            Code.Add("sk");
+            Code.Add("fi");
+            Code.Add("sl");
+            Code.Add("fr");
+            Code.Add("es");
+            Code.Add("gl");
+            Code.Add("sw");
+            Code.Add("ka");
+            Code.Add("sv");
+            Code.Add("de");
+            Code.Add("ta");
+            Code.Add("el");
+            Code.Add("te");
+            Code.Add("gu");
+            Code.Add("th");
+            Code.Add("ht");
+            Code.Add("tr");
+            Code.Add("iw");
+            Code.Add("uk");
+            Code.Add("hi");
+
+            Code.Add("hu");
+            Code.Add("vi");
+            Code.Add("is");
+            Code.Add("cy");
+            Code.Add("id");
+            Code.Add("yi");
+
+            Random rand = new Random();
+            int index = rand.Next(Code.Count);
+            string c = (Code[index]);
+            return c;
+        }
+        public string getcode(string code)
+        {
+
+            Dictionary<string, string> Languages = new Dictionary<string, string>();
+            Languages.Add("af", "Afrikaans");
+            Languages.Add("ga", "Irish");
+            Languages.Add("sq", "Albanian");
+            Languages.Add("it", "Italian");
+
+            Languages.Add("ja", "Japanese");
+
+            Languages.Add("kn", "Kannada");
+            Languages.Add("eu", "Basque");
+            Languages.Add("ko", "Korean");
+            Languages.Add("bn", "Bengali");
+            Languages.Add("la", "Latin");
+            Languages.Add("be", "Belarusian");
+            Languages.Add("lv", "Latvian");
+            Languages.Add("bg", "Bulgarian");
+            Languages.Add("lt", "Lithuanian");
+            Languages.Add("ca", "Catalan");
+            Languages.Add("mk", "Macedonian");
+            Languages.Add("zh-CN", "ChineseSimplified");
+            Languages.Add("ms", "Malay");
+            Languages.Add("zh-TW", "ChineseTraditional");
+            Languages.Add("mt", "Maltese");
+            Languages.Add("hr", "Croatian");
+            Languages.Add("no", "Norwegian");
+            Languages.Add("cs", "Czech");
+            Languages.Add("fa", "Persian");
+            Languages.Add("da", "Danish");
+            Languages.Add("pl", "Polish");
+            Languages.Add("nl", "Dutch");
+            Languages.Add("pt", "Portuguese");
+            
+            Languages.Add("ro", "Romanian");
+            Languages.Add("eo", "Esperanto");
+            Languages.Add("ru", "Russian");
+            Languages.Add("et", "Estonian");
+            Languages.Add("sr", "Serbian");
+            Languages.Add("tl", "Filipino");
+            Languages.Add("sk", "Slovak");
+            Languages.Add("fi", "Finnish");
+            Languages.Add("sl", "Slovenian");
+            Languages.Add("fr", "French");
+            Languages.Add("es", "Spanish");
+            Languages.Add("gl", "Galician");
+            Languages.Add("sw", "Swahili");
+            Languages.Add("ka", "Georgian");
+            Languages.Add("sv", "Swedish");
+            Languages.Add("de", "German");
+            Languages.Add("ta", "Tamil");
+            Languages.Add("el", "Greek");
+            Languages.Add("te", "Telugu");
+            Languages.Add("gu", "Gujarati");
+            Languages.Add("th", "Thai");
+            Languages.Add("ht", "HaitianCreole");
+            Languages.Add("tr", "Turkish");
+            Languages.Add("iw", "Hebrew");
+            Languages.Add("uk", "Ukrainian");
+            Languages.Add("hi", "Hindi");
+
+            Languages.Add("hu", "Hungarian");
+            Languages.Add("vi", "Vietnamese");
+            Languages.Add("is", "Icelandic");
+            Languages.Add("cy", "Welsh");
+            Languages.Add("id", "Indonesian");
+            Languages.Add("yi", "Yiddish");
+
+
+            string xmlfile = Languages[code.ToString()];
+            code = xmlfile;
+            return code;
+        }
+    }
+}
---
+++ SharePostsTutorial/Program.cs
@@ -0,0 +1,181 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace SharePostsTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PutPost();
+            Environment.Exit(0);
+        }
+        public static void PutPost()
+        {
+            Task<string> task = GetQuote();
+            task.Wait();
+            var x = task.Result;
+            string quote = x;
+            string json = @"{
+                ""author"": ""urn:li:person:PERSONID"",
+                ""lifecycleState"": ""PUBLISHED"",
+                ""specificContent"": {
+                ""com.linkedin.ugc.ShareContent"": {
+                ""shareCommentary"": {
+                ""text"": ""POSTDATA""
+                  },
+                ""shareMediaCategory"": ""NONE""
+                }
+                },
+                ""visibility"": {
+                ""com.linkedin.ugc.MemberNetworkVisibility"": ""PUBLIC""
+                 }
+                }";
+
+            string jsonupdate = json.Replace("POSTDATA", quote);
+            string jsonupdate1 = jsonupdate.Replace("PERSONID", Properties.Settings.Default.LinkedinAuthor.ToString());
+            task.Dispose();
+
+            Task<string> tasks = PutQuotes(jsonupdate1);
+            tasks.Wait();
+            var xx = tasks.Result;
+            task.Dispose();
+
+        }
+
+        public static string TranslateText(string code, string text)
+        {
+
+            string url = String.Format
+            ("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}",
+             "en", code.ToString(), Uri.EscapeUriString(text.ToString()));
+            HttpClient httpClient = new HttpClient();
+            string result = httpClient.GetStringAsync(url).Result;
+            var jsonData = new JavaScriptSerializer().Deserialize<List<dynamic>>(result);
+            var translationItems = jsonData[0];
+            string translation = "";
+            foreach (object item in translationItems)
+            {
+
+                IEnumerable translationLineObject = item as IEnumerable;
+                IEnumerator translationLineString = translationLineObject.GetEnumerator();
+                translationLineString.MoveNext();
+                translation += string.Format(" {0}", Convert.ToString(translationLineString.Current));
+            }
+
+
+            if (translation.Length > 1) { translation = translation.Substring(1); };
+            return translation;
+        }
+
+        public class Root
+        {
+            public string q { get; set; }
+            public string a { get; set; }
+            public string h { get; set; }
+        }
+
+        static async Task<string> PutQuotes(string jsonupdate)
+        {
+            Newtonsoft.Json.Linq.JObject o1 = JObject.Parse(jsonupdate);
+            var contents = o1;
+
+            var jsonContent = JsonConvert.SerializeObject(contents);
+
+            string baseUrl = "https://api.linkedin.com/v2/ugcPosts";
+
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders
+                    .Accept
+                    .Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, baseUrl);
+
+            requestMessage.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            requestMessage.Headers.Add("Authorization", "Bearer " + Properties.Settings.Default.LinkedinToken.ToString());
+
+
+            HttpResponseMessage responseMessage = await httpClient.SendAsync(requestMessage);
+
+            string responseAsString = await responseMessage.Content.ReadAsStringAsync();
+            if (responseAsString != null)
+            {
+                Console.WriteLine(responseAsString);
+            }
+            return responseAsString;
+        }
+
+
+        static async Task<string> GetQuote()
+        {
+
+            GoogleLang g = new GoogleLang();
+            string code = g.codelist();
+            string ConvertedEndMessage = g.getcode(code);
+            string Post = "";
+            string baseUrl = "https://zenquotes.io/api/random";
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, baseUrl);
+            HttpResponseMessage responseMessage = await httpClient.SendAsync(requestMessage);
+            var responseAsString = await responseMessage.Content.ReadAsStringAsync();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Root[] quoteResponse = js.Deserialize<Root[]>(responseAsString);
+            string quote = quoteResponse[0].q.TrimEnd();
+            string author = quoteResponse[0].a;
+            string GoogleTranslate = "";
+            TimeSpan time = DateTime.Now.TimeOfDay;
+
+
+            string EndMessage = "";
+            if (time > new TimeSpan(00, 00, 01)
+             && time < new TimeSpan(12, 00, 00))
+            {
+                EndMessage = Properties.Settings.Default.Morning.ToString();
+                GoogleTranslate = TranslateText(code, EndMessage);
+
+            }
+            if (time > new TimeSpan(12, 00, 00)
+          && time < new TimeSpan(18, 00, 00))
+            {
+                EndMessage = Properties.Settings.Default.Noon.ToString();
+                GoogleTranslate = TranslateText(code, EndMessage);
+
+            }
+            if (time > new TimeSpan(18, 00, 00)
+      )
+            {
+                EndMessage = Properties.Settings.Default.Evening.ToString();
+                GoogleTranslate = TranslateText(code, EndMessage);
+
+            }
+            if (EndMessage == "")
+            {
+                EndMessage = "Have a great day all -";
+            }
+            if (GoogleTranslate != "")
+            {
+
+                Post = ("'" + quote + "'" + "\n" + "\n" + author + "\n" + "\n" + "#" + ConvertedEndMessage + " - " + GoogleTranslate + " " + " - (" + EndMessage + ")" + "\n" + "\n" + "#Learn" + ConvertedEndMessage + Properties.Settings.Default.HashTags);
+
+            }
+            else Post = ("'" + quote + "'" + "\n" + "\n"  + author + "\n" + "\n" + EndMessage + "\n" + "\n" + Properties.Settings.Default.HashTags);
+            return Post;
+
+
+
+        }
+
+    }
+}
